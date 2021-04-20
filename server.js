936655bd80caea8e59f9c298b70ec4b096a89d34
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2021-04-20 23:22:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
   
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -27,24 +27,18 @@ app.use(function (req, res, next) {
   });
 
 app.get('/', function (req, res) {
-    request('https://api.npoint.io/20c1afef1661881ddc9c', function (error, response, body) {
+    request('https://raw.githubusercontent.com/CodellionSolution/newapp-server/main/newsapp.json', function (error, response, body) {
         console.error('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', typeof(body)); // Print the HTML for the Google homepage.
-        var array =[]
-        var obj = JSON.parse(body);
-
-        for (var i = 0; i < obj["playerList"].length; i++)
-        {
-            array.push({"image":"assets/"+obj["playerList"][i]["Id"]+".jpg","PFName":obj["playerList"][i]["PFName"],"Value":obj["playerList"][i]["Value"],"VsCCode":obj["playerList"][i]["UpComingMatchesList"][0]["VsCCode"],"CCode":obj["playerList"][0]["UpComingMatchesList"][0]["CCode"],"date":obj["playerList"][0]["UpComingMatchesList"][0]["MDate"],"skilldesc":obj["playerList"][i]["SkillDesc"],"TeamName":obj["playerList"][i]["TName"]})
     
-        }
 
+       
        
         
       
         res.contentType('application/json');
-        res.send(JSON.stringify(array));
+        res.send(body);
 
       });
       
